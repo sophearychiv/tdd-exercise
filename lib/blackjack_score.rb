915ee0fc56a8075f0cc1c487d9c8eb75c4f2e4f8
Sopheary Chiv <sophearychiv@gmail.com>
2019-02-20 13:55:20 -0800

    blackjack_score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,17 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
+hand = VALID_CARDS.shuffle.first(2)
 
 def blackjack_score(hand)
-
+  # hand with 2 cards
+  score = 0
+  hand.each do |i|
+    if i == "King" || i == "Queen" || i == "Jack"
+      score = score + 10
+    else
+      score = score + i
+    end
+  end
+  return score
 end
